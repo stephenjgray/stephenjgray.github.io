24334c58f6ff3e875d74a1a5e62bb0128d56387f
Stephen Gray <stephenjgray79@gmail.com>
2020-01-31 14:47:53 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {
+  Root,
+  Sidebar,
+  Content,
+  CollapseBtn,
+  CollapseIcon
+} from "@mui-treasury/layout";
+import fixedLayoutPreset from "@mui-treasury/layout/presets/fixed";
+
+import NavContent from "./components/NavContent";
+import NavHeader from "./components/NavHeader";
+
+import cvData from "./assets/cv.json";
+import Main from "./components/Main";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root config={fixedLayoutPreset}>
+      {({ sidebarStyles }) => (
+        <>
+          <CssBaseline />
+          <Content>
+            <Main data={cvData} />
+          </Content>
+          <Sidebar style={{ backgroundColor: '#fafafa'}}>
+            {({ collapsed }) => (
+              <>
+                <NavHeader
+                  style={{ overflowX: 'hidden' }}
+                  data={{ profile: cvData.cv.profile, collapsed: collapsed }}
+                />
+                <div className={sidebarStyles.container}>
+                  <NavContent style={{ overflowX: 'hidden' }} 
+                  data={{ collapsed: collapsed }} />
+                </div>
+                <CollapseBtn className={sidebarStyles.collapseBtn}>
+                  <CollapseIcon />
+                </CollapseBtn>
+              </>
+            )}
+          </Sidebar>
+        </>
+      )}
+    </Root>
   );
 }
 
