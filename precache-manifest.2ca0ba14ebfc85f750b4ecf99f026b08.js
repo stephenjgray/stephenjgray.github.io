75f76158edbbe9efa8728cafb287b37d6275c2d5
Stephen Gray <stephenjgray79@gmail.com>
2020-01-31 14:49:35 +0000

    Updates

---
+++ precache-manifest.2ca0ba14ebfc85f750b4ecf99f026b08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a774185132181fad7a42bc8aa1d83e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e74717f9a488d56055c",
+    "url": "/static/css/main.2669f1a9.chunk.css"
+  },
+  {
+    "revision": "b1fd87b932cac8ec081d",
+    "url": "/static/js/2.3900e8cf.chunk.js"
+  },
+  {
+    "revision": "65cba7ba6c80630c249ee1be9d093412",
+    "url": "/static/js/2.3900e8cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e74717f9a488d56055c",
+    "url": "/static/js/main.ea6293dc.chunk.js"
+  },
+  {
+    "revision": "1cb0b4b752e3b6222891",
+    "url": "/static/js/runtime-main.c3619568.js"
+  },
+  {
+    "revision": "097ddabac727ad4c70ecd872bc726c98",
+    "url": "/static/media/max-nelson-Se9ufo2dO_g-unsplash2.097ddaba.jpg"
+  },
+  {
+    "revision": "894e5681ab137bb36ae9365102def2a4",
+    "url": "/static/media/steve.894e5681.png"
+  }
+]);
