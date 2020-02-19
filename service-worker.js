c4acd69c6197b2376928c9d9074a48bf3b72ef29
Stephen Gray <stephenjgray79@gmail.com>
2020-02-19 15:52:24 +0000

    Adding CV and updating links, removing dead code

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cd5c66a13c16c21c34e027478a93f62.js"
+  "/precache-manifest.e235833d93f20987c5114d1f29327d30.js"
 );
 
 self.addEventListener('message', (event) => {
